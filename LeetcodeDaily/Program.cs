a8ac93f2a79371403039dd22b46ae9404bed8765
Chí Sơn <caosychison3011@gmail.com>
2025-06-24 21:09:38 +0700

    24/06/2025 - Eazy but cann't fix bug

--- LeetcodeDaily/Program.cs
+++ LeetcodeDaily/Program.cs
@@ -1,70 +1,30 @@
 ﻿using System;
-
 public class Solution
 {
-    public long ChangeBase(long n, long k)
+    public IList<int> FindKDistantIndices(int[] nums, int key, int k)
     {
-        string digit = "";
-        while (n > 0)
+        List<int> result = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
         {
-            long ans = n % k;
-            digit = ans.ToString() + digit;
-            n = n / k;
-        }
-        return long.Parse(digit);
-    }
-
-    public bool IsPalindrome(long n)
-    {
-        string check = n.ToString();
-        char[] chars = check.ToCharArray();
-        Array.Reverse(chars);
-        string reverse = new string(chars);
-        return check == reverse;
-    }
-    public long KMirror(long k, long n)
-    {
-        long count = 0;
-        long sum = 0;
-        long numLength = 1;
-        while (true)
-        {
-            long x = (long)Math.Ceiling((double)numLength / 2);
-            long start = (long)Math.Pow(10, x - 1);
-            long end = (long)Math.Pow(10, x);
-            for (long i = start; i < end; i++)
-            {
-                string full;
-                string temp = i.ToString();
-                if (numLength % 2 == 0)
-                {
-                    full = temp + new string(temp.Reverse().ToArray());
-                }
-                else
-                {
-                    string reverse = new string(temp.Reverse().ToArray());
-                    full = temp + reverse.Substring(1);
-                }
-                long num = long.Parse(full);
-                if (IsPalindrome(ChangeBase(num, k)))
-                {
-                    sum += num;
-                    count++;
-                    if (count == n)
-                        return sum;
-                }
+            for (int j = 0; j < nums.Length; j++)
+            {  
+                    if (nums[j] == key && Math.Abs(i - j) <= k)
+                    {
+                        result.Add(i);
+                        break;
+                    }
             }
-            numLength++;
         }
-
+        return result;
     }
 }
 class Program
 {
     static void Main(string[] args)
     {
-        Solution solution = new Solution();
-        long result = solution.KMirror(2, 5);
-        Console.WriteLine(result);
+        
+        Solution s = new Solution();
+        var result = s.FindKDistantIndices([3, 4, 9, 1, 3, 9, 5], 9, 1);
+        Console.WriteLine(string.Join(", ", result));
     }
 }
