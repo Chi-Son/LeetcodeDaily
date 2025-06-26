fe66685537206fd5ec29b21cd2212efc4caef246
Chí Sơn <caosychison3011@gmail.com>
2025-06-26 21:17:15 +0700

    26/06/2025 Med Learn how to increase bitPostion in Binary (1L<<)

--- LeetcodeDaily/Program.cs
+++ LeetcodeDaily/Program.cs
@@ -1,30 +1,41 @@
 ﻿using System;
 public class Solution
 {
-    public IList<int> FindKDistantIndices(int[] nums, int key, int k)
+    public int LongestSubsequence(string s, int k)
     {
-        List<int> result = new List<int>();
-        for (int i = 0; i < nums.Length; i++)
-        {
-            for (int j = 0; j < nums.Length; j++)
-            {  
-                    if (nums[j] == key && Math.Abs(i - j) <= k)
+        int count = 0;
+        long value = 0;
+        int bitPosition = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == '0')
+                {
+                    count++;
+                }
+                else
+                {
+                if (bitPosition < 63)
+                {
+                    long add = 1L << bitPosition;
+                    if (value + add <= k)
                     {
-                        result.Add(i);
-                        break;
+                        value += add;
+                        count++;
                     }
+                }
             }
+                if (bitPosition <63)
+                    bitPosition++;
+            }
+            return count;
         }
-        return result;
-    }
 }
 class Program
 {
     static void Main(string[] args)
     {
-        
         Solution s = new Solution();
-        var result = s.FindKDistantIndices([3, 4, 9, 1, 3, 9, 5], 9, 1);
-        Console.WriteLine(string.Join(", ", result));
+        Console.WriteLine(s.LongestSubsequence("1001010", 5));
     }
 }
