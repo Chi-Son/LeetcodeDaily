4fd37eaf5de1fba40de97ad628f11fbdc16f0381
Chí Sơn <caosychison3011@gmail.com>
2025-06-27 20:56:08 +0700

    27/06/2025 Leetcode 386

--- LeetcodeDaily/Program.cs
+++ LeetcodeDaily/Program.cs
@@ -1,41 +1,38 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices.Marshalling;
 public class Solution
 {
-    public int LongestSubsequence(string s, int k)
+    public IList<int> LexicalOrder(int n)
     {
-        int count = 0;
-        long value = 0;
-        int bitPosition = 0;
-
-        for (int i = s.Length - 1; i >= 0; i--)
+        int current = 1;
+        List<int> l = new List<int>();
+        for (int i = 0; i < n; i++) {
+            l.Add(current);
+            if (current * 10 <= n)
             {
-                if (s[i] == '0')
-                {
-                    count++;
-                }
-                else
-                {
-                if (bitPosition < 63)
-                {
-                    long add = 1L << bitPosition;
-                    if (value + add <= k)
-                    {
-                        value += add;
-                        count++;
-                    }
-                }
+                current *= 10;
             }
-                if (bitPosition <63)
-                    bitPosition++;
+            else
+            {
+                while (current%10==9|| current + 1 > n)
+                {
+                    current /= 10;
+                }
+                current++;
             }
-            return count;
         }
+        return l;
+    }
 }
 class Program
 {
     static void Main(string[] args)
     {
         Solution s = new Solution();
-        Console.WriteLine(s.LongestSubsequence("1001010", 5));
+        foreach (int num in s.LexicalOrder(30))
+        {
+            Console.Write(num + " ");
+        }
     }
 }
